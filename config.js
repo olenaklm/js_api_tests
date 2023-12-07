456da574faa5cd9f36e7bf43c0bc461120c44383
Olena_Klymenko <lenaklymenko9@gmail.com>
2023-12-07 13:40:12 +0200

    Changed cf

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    accessToken: 'sl.BrTHlgtBsAA_W_UEmuX-n4JOy3rBLd0GGxaa8LS_h3NRazTcYzvEVaSEDDOCdMEeu6ri2hOA_5cfLdTltX811sTexXR-E-4CzFB_SKTwUuAh9S9klvX-bk8I656fswCmQvHjo6y2uMZC'
+    accessToken: 'sl.BrRBvGUFp64RSm6hvm_jn7W4E9Yjvw5iejEv9H2Pb117jwxeAlAhqixWzMR-Qzb8nuThi5C0cn8SAaRDVMCB4Mf58m1DbjgiKkYIrW3mO7H2Ya62N9j1UcuPHGwbwqoRuWweQX9_YAsN'
   };
